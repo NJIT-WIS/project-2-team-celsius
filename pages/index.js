8a7e6c4416281f9d3241ce226fa26567a599ff20
KDI2 <ki6@njit.edu>
2023-04-13 16:29:08 -0400

    index.test.update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
-        <title>{Your name}</title>
+        <title>{Yourname}</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
